ec8051d33fb1075df9df628a78933440edb26c36
Imemyslf <kishanbro9666@gmail.com>
2024-10-26 22:52:19 +0530

    Updated code

--- Code/findTable.py
+++ Code/findTable.py
@@ -3,77 +3,72 @@ from bs4 import BeautifulSoup
 import pandas as pd  
 import os
 
-
-with open("../data/Oil and Natural Gas Corporation Ltd/Quarterly10Yrs/9_Jun23_Jun24.html", "r", encoding='utf-8') as f:
-    html_content = f.read()
-
-soup = BeautifulSoup(html_content, "html.parser")
-
-company_soup_name = soup.find(class_="pcstname")
-company_name = company_soup_name.get_text()
-
-print(company_name,"\n")
-
-sector_class = soup.find(class_="hidden-lg")
-sector_name = sector_class.get_text().split()
-
-print(sector_name)
-final_sector_name = " ".join(sector_name)
-print(final_sector_name,"\n")
-
-try:
-    os.mkdir("../Companies")
-    print("Companies directory created successfully")
-except FileExistsError as e:
-    print("File already exists or Error in creating directory",e) 
-except PermissionError as e:
-    print("Permission denied",e)
-except Exception as e:
-    print("An Error Occured while creating the directory",e)  
-
-with open("../Companies/sector.txt","w") as f:
-    f.write(str(final_sector_name))
-
-try: 
-    os.mkdir(f"../Companies/{final_sector_name}")
-    print(f"{final_sector_name} directory created successfully")
-except FileExistsError as e:
-    print("File already exists or Error in creating directory",e) 
-except PermissionError as e:
-    print("Permission denied",e)
-except Exception as e:
-    print("An Error Occured while creating the directory",e)
-    
-try: 
-    os.mkdir(f"../Companies/{final_sector_name}/{company_name}")
-    print(f"{company_name} directory created successfully")
-except FileExistsError as e:
-    print("File already exists or Error in creating directory",e) 
-except PermissionError as e:
-    print("Permission denied",e)
-except Exception as e:
-    print("An Error Occured while creating the directory",e)
-
-
-
-
-# name = sector_class.find(text="Sector")
-# print(name)
-
-# table = soup.find(class_="mctable1")
-
-# table_data = []
-
-# rows = table.find_all("tr")
-
-# for i, row in enumerate(rows):    
-#     cols = row.find_all(["td", "th"])  
-#     col_text = [col.get_text(strip=True) for col in cols]  
+def make_main_dir(base_dir):
+    try:
+        os.mkdir(base_dir)
+        print("Companies directory created successfully")
         
-#     table_data.append(col_text)
+    except FileExistsError as e:
+        print("File already exists in  directory") 
+        
+    except PermissionError as e:
+        print("Permission denied",e)
+        return False
+    except Exception as e:
+        print("An Error Occured while creating the directory",e)
+        return False
 
-# df = pd.DataFrame(table_data[1:], columns=table_data[0])  
 
-# df.to_excel("../data/Reliance/QuarterlyExcel/9_Sep23_Sep24.xlsx", index=False)  
+def append_sector(final_sector_name):
+    path = "../Companies/sector.txt"
+    with open(path,"w") as f:
+        f.write(str(final_sector_name))
 
-# print("Table saved to/9_Sep23_Sep24.xlsx")
+
+def create_company_directory(directory):    
+    try: 
+        os.makedirs(directory)
+        print(f"{directory} directory created successfully")
+    except FileExistsError as e:
+        print("File already exists or Error in creating directory",e) 
+    except PermissionError as e:
+        print("Permission denied",e)
+        return False
+    except Exception as e:
+        print("An Error Occured while creating the directory",e)
+        return False
+
+
+def create_excel_file(path,base_dir,file_name):
+    print("Creating")
+    with open(path,"r",encoding="utf-8") as f:
+        html_content = f.read()
+    
+    soup = BeautifulSoup(html_content,"html.parser")
+    # print(soup.prettify())
+    
+    table = soup.find(class_="mctable1")
+    # print(table)
+    
+    table_data = []
+    rows = table.find_all("tr")
+    
+    for i,rows in enumerate(rows):  
+        cols = rows.find_all(["td","th"])
+        col_text =[ col.get_text(strip="\n\t") for col in cols]
+        table_data.append(col_text)
+        
+    df = pd.DataFrame(table_data[1:], columns=table_data[0])  
+    
+    
+    new_path = "../data/AdaniPorts/QuarterlyExcel"
+    output = create_company_directory(new_path)
+    if output != False:
+        df.to_excel(f"{base_dir}/QuarterlyExcel/{file_name}", index=False)
+        
+        
+if __name__ == "__main__": 
+    path = "../data/AdaniPorts/Quarterly10Yrs/9_Jun23_Jun24.html" 
+    base_dir = "../data/AdaniPorts"
+    file_name = "9_Sep23_Sep24.xlsx"  
+    create_excel_file(path,base_dir,file_name)
--- Code/main.py
+++ Code/main.py
@@ -1,10 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
+from findTable import append_sector,make_main_dir,create_company_directory
+import os
+import re
 
-def fetchandSave(url, path):
-    r = requests.get(url)
-    
+def fetchandSave(url, path):  
+    r = requests.get(url)  
     with open(path, "w", encoding="utf-8") as f:
         f.write(r.text)
 
-url = "https://www.moneycontrol.com/financials/oilnaturalgascorporation/results/quarterly-results/ONG#ONG"
-fetchandSave(url, "../data/Oil and Natural Gas Corporation Ltd/Quarterly10Yrs/9_Jun23_Jun24.html")
+url = "https://www.moneycontrol.com/financials/adaniportsspecialeconomiczone/results/quarterly-results/MPS#MPS"
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content,"html.parser")
+
+company_soup_name = soup.find(class_="pcstname")
+company_name = company_soup_name.get_text().strip()
+
+print("Company Name:-",company_name,"\n")
+
+sector_class = soup.find(class_="hidden-lg")
+sector_name = sector_class.get_text().split()
+
+print(sector_name)
+final_sector_name = " ".join(sector_name).strip()
+print("Sector Name:-",final_sector_name,"\n")
+
+base_dir = "../Companies"
+file_creation = make_main_dir(base_dir)
+path = "../data/AdaniPorts/Quarterly10Yrs/9_Jun23_Jun24.html"
+if file_creation != False:
+    append_sector(final_sector_name)
+    create_company_directory(os.path.join(base_dir,final_sector_name,company_name))
+
+fetchandSave(url, path)
