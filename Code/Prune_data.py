1f875727ad4953bb522fa3b54bf429e15b92045d
Imemyslf <kishanbro9666@gmail.com>
2024-11-10 09:41:09 +0530

    Combined the excel file with quaters

--- Code/Prune_data.py
+++ Code/Prune_data.py
@@ -8,12 +8,14 @@ from openpyxl.styles import Alignment
 current_dir = os.getcwd()
 path = f"{current_dir}/Companies"
 final_quaters = []
-
+quater_Q = [""]
+final_data_list = []
+k = True
 def prune_data(sector_name,company_name,file):
-    global current_dir
-    print(current_dir)
+    global current_dir,quater_Q,final_quaters,k
+    # print(current_dir)
     
-    file_name = f"{current_dir}/Companies/{sector_name}/{company_name}/Excel/{file}"
+    file_name = f"{current_dir}/Companies/{sector_name}/{company_name}/{file}"
     
     df = pd.read_excel(file_name)
 
@@ -21,11 +23,23 @@ def prune_data(sector_name,company_name,file):
     #print(col_names)
     
     quaters = col_names[1:]
-    quaters.reverse()
-    #print(quaters)
+    # quaters.reverse()
+    # print((quaters))
+    j = 1
     
-    for i in quaters:
-        final_quaters.append(i)
+    if k :
+        for i in range(0,(int(len(quaters)/5))):
+            qua = quaters[(i*5) : (i*5) + 5]
+            qua.reverse()
+            # print(qua,"\n")
+            for i in qua:
+                quater_Q.append(f"Q{j}")
+                j += 1
+                final_quaters.append(i)    
+    
+    k = False
+    print(final_quaters)
+    print(quater_Q)
     
     # print(f"\nfilename {file} \n quaters{quaters}")
     # print(final_quaters)
@@ -43,17 +57,66 @@ def prune_data(sector_name,company_name,file):
     
     def isValid(data):
         return not ( data == '--')
+    
+    final_values = []
+    i = 0
+    for value in data_list:
+        # if i == 0:
+            parameter , *values = value
+            # print("\n",values)
+            for i in range(0,9):
+                val = values[(i*5) : (i*5) + 5]
+                val.reverse()
+                # print("\n\n Parameter:-  ",parameter,"Val:- ",val)
+                
+                for i in val:
+                    final_values.append(i)
+        # else:
+        #     break
+    
+    # print("\n\n Final value",len(final_values))
+    # print(len(data_list))
+    # print("\n\n Data List:- ",data_list[0])
+    
+    j = 0
+    for i in range(0,len(data_list)):
+        data = data_list[i]
+        data_1  = data.copy()
+        # print(data_1)
+        
+        data_1.clear()
+        
+        data_1.append(final_values[j:j+45])
+        j += 45
+       
+        # print(data_1)
+        flattend_list = data_1[0]
+        # print(flattend_list)
+        
+        
+        final_data = [data[0]] + flattend_list
+        # print(final_data)
+        data_list[i] = final_data
+        # print("\n\n Data list:- ",data_list[i])
+        
+        
+        # print("\n\n================================")
+        # print(data_list)
+        # final_data_list.append(data_list[i])
+        # print("\n\n",final_data_list)
+    
 
     for row in data_list:
         company, *values = row
-        values.reverse()
+        # print("\n Values inside row revsered:- ",value)
         if any(isValid(value) for value in values):
             output_data.append([company] + values)
-            # dict[company] = values
+            dict[company] = values
 
     # print("Output data before :-",output_data)
     # output_data = output_data[1:]
     # print("Output data after :-",output_data[1:])
+
 
     def isValids(val):
         if(str(val) == "nan" ):
@@ -63,10 +126,8 @@ def prune_data(sector_name,company_name,file):
         
     for row in output_data:
         paramter, *values = row
-        print("\nValue before:- ",values)
+        # print("\nValue before:- ",values)
         #values.append("")
-        
-        print("\nValues after:- ",values)
         if any(isValids(value) for value in values):
             param.append([""])
             param.append([paramter] + values)
@@ -84,21 +145,26 @@ def prune_data(sector_name,company_name,file):
             param.append([paramter] + values + [])
 
     
-    print("\nData:- \n",param)
+    param.insert(0,quater_Q)
+    # print("\nData:- \n",param)
 
     col_name = col_names[0]
-    #quaters = col_names[1:]
 
     print(col_name,quaters)
     output_file_path = f"{current_dir}/Companies/{sector_name}/{company_name}/Pruned_Excel/{file}"
     print("\n\nCurrent dir:- ",output_file_path)
-    
-    output_df = pd.DataFrame(param,columns=[str(col_name)]+quaters)
+    deleteExtraCell(output_file_path)
+    output_df = pd.DataFrame(param,columns=[str(col_name)]+final_quaters)
     
 
     output_df.to_excel(output_file_path,index=False)
     print("\n\n\nSuccessfully saved the file in:- ",output_file_path)
     spacexcel(output_file_path,sector_name,company_name,file)
+
+
+def deleteExtraCell(output_file_path):
+    print("Inside delete cel; ",output_file_path)
+    pass
 
 def spacexcel(output_file_path,sector_name,company_name,file):
     global current_dir
@@ -149,34 +215,35 @@ def prune_folders():
         
         for i in range(len(comapnies_list)):
             company = comapnies_list[i]
-            # print(f"Company {i}:- {company}")
+            print(f"Company {i}:- {company}")
             # path = f"{current_dir}/Companies"
-            path = f"{current_dir}/Companies/{sector}/{company}/Excel"
-            print("\nFinal path to  excel:-",path)
-    
-            dir_list = os.listdir(path)
+            path = f"{current_dir}/Companies/{sector}/{company}"
+            print("\nFinal path to excel:-",path)
+
+            prune_data(sector,company,"combined_excel_file.xlsx")
+            # dir_list = os.listdir(path)
             
-            for dir in dir_list:
-                print("\n\nFile name:- ",dir,"\n")
-                prune_data(sector,company,dir)
-    pass
+            # for dir in dir_list:
+            #     print("\n\nFile name:- ",dir,"\n")
+            #     prune_data(sector,company,dir)
+    
 
 if __name__ == '__main__':    
     
-    # prune_folders()
+    prune_folders()
     
-    sector_name = "Trading"
-    company_name  = "Adani Enterprises Ltd"
-    file = "1_Sep13_Sep14.xlsx"
+    # sector_name = "Refineries"
+    # company_name  = "Bharat Petroleum Corporation Ltd"
+    # file = "combined_excel_file.xlsx"
     
-    # path = f"{current_dir}/Companies/{sector_name}/{company_name}/Excel"
-    # kisu_list = os.listdir(path)
-    # #print(kisu_list)
+    # # path = f"{current_dir}/Companies/{sector_name}/{company_name}/Excel"
+    # # kisu_list = os.listdir(path)
+    # # #print(kisu_list)
     
-    # for kisu in kisu_list:
-    #     #print(kisu)
-    #     prune_data(sector_name,company_name,kisu)
-    prune_data(sector_name,company_name,file)
+    # # for kisu in kisu_list:
+    # #     #print(kisu)
+    # #     prune_data(sector_name,company_name,kisu)
+    # prune_data(sector_name,company_name,file)
     
     
         
