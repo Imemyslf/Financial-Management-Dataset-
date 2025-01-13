76f7f688d31563244e84955603c7e4556d878efb
Imemyslf <kishanbro9666@gmail.com>
2025-01-13 23:20:13 +0530

    Working with tbales

--- Code/tabular.py
+++
@@ -1,8 +0,0 @@
-import tabula
-
-pdf_path = r"C:\\Users\\rayya\\Desktop\\All-Files\\Programs\\FYP\\Financial-Management-Dataset-\\Code\\tata.pdf"
-
-dfs = tabula.read_pdf(pdf_path, pages='17')
-
-print((len(dfs)))
-print(dfs[0])
